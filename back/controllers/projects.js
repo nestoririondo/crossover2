e8e91580898e571e12bb46439ee9bcd2b122fef4
Néstor Iriondo <nestoririondo@gmail.com>
2024-02-19 13:08:56 +0100

    donate endpoint

--- back/controllers/projects.js
+++ back/controllers/projects.js
@@ -36,3 +36,19 @@ export const createProject = async (req, res) => {
     res.json({ message: error.message });
   }
 };
+
+export const createDonation = async (req, res) => {
+  const { user_id, project_id, amount } = req.body;
+  try {
+    const project = await Project.findById(project_id).populate("donations"); // populate the donations array with the user data
+    if (!project) {
+      return res.status(404).json({ message: "Project not found." });
+    }
+    const donation = { user: user_id, amount };
+    project.donations.push(donation);
+    await project.save();
+    res.json(project);
+  } catch (error) {
+    res.json({ message: error.message });
+  }
+};
