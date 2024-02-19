fbe6f1fec2f31d28c72b51abb98bf86233eb8af6
issaali <issa.alali.991@gmail.com>
2024-02-19 13:18:36 +0100

    add create user

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -7,6 +7,7 @@ import Project from "./components/Project";
 
 // Router
 import { Routes, Route } from "react-router-dom";
+import SignupForm from "./components/SignupForm";
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
       <Routes>
         <Route path="/" />
         <Route path="/project/:id" element={<Project />} />
+        <Route path="/signup" element={<SignupForm />} />
       </Routes>
     </>
   );
