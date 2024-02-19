bc2abf832018c391c9debcce1f1da981dbad73c0
kenkindermann <ken.kindermann@gmail.com>
2024-02-19 10:23:36 +0100

    install react-router-dom, add routes to app.jsx, implement project component

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,5 +1,20 @@
+// Components
+import Navbar from "./components/Navbar";
+import Project from "./components/Project";
+
+// Router
+import { Routes, Route } from "react-router-dom";
+
 function App() {
-  return <>Hi</>;
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" />
+        <Route path="/project/:id" element={<Project />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
---
+++ front/src/components/Project.jsx
@@ -0,0 +1,27 @@
+const ProjectView = () => {
+  return (
+    <section id="projectView">
+      <h1>Support my open source social media app for cats</h1>
+      <div>
+        <img src="" alt="" />
+        <div>
+          <ul>
+            <li>Raised</li>
+            <li>Goal</li>
+          </ul>
+          <button>Donate</button>
+        </div>
+      </div>
+      <div className="description">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor facere eius maxime hic molestiae libero rerum, id
+        nisi corporis dicta debitis possimus accusantium officia quasi amet nihil! Quasi, optio enim? Commodi numquam
+        iste minus quo dignissimos ex laboriosam ipsam a! Unde, reprehenderit porro! Corporis suscipit explicabo sunt
+        facere odit accusantium eaque aperiam ex. Numquam, officiis animi! Dignissimos, rerum repellat. Ab? Ex eum in
+        blanditiis fuga. Aperiam, esse ratione dolore ab maxime earum et libero exercitationem quas odio. Quod, saepe
+        molestias non error numquam eligendi aliquid! Laborum neque nostrum rerum et. Possimus omnis harum molestias
+      </div>
+    </section>
+  );
+};
+
+export default ProjectView;
