d00790f088d3b83d0caf10a121a903e45b06fe54
Néstor Iriondo <nestoririondo@gmail.com>
2024-02-19 13:22:58 +0100

    checkUserExists middleware

--- back/routes/usersRouter.js
+++ back/routes/usersRouter.js
@@ -1,11 +1,11 @@
 import { Router } from "express";
 import { registerUser, loginUser, getUser } from "../controllers/users.js";
-import { checkData } from "../middlewares/checkData.js";
+import { checkData, checkUserExists } from "../middlewares/users.js";
 import { authenticate } from "../middlewares/authenticate.js";
 
 const usersRouter = Router();
 
-usersRouter.post("/", checkData, registerUser);
+usersRouter.post("/", checkData, checkUserExists, registerUser);
 usersRouter.post("/login", loginUser);
 usersRouter.get("/user", authenticate, getUser);
 
